d4162cf1bad1b99a1268d69631c7204097be72d3
zxckoii <145819507+prdhk2@users.noreply.github.com>
2024-04-23 11:48:22 +0700

    u

---
+++ application/controllers/Login_c.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login_c extends CI_Controller {
+
+	public function index()
+	{
+        $this->load->view('login/login.php');
+    }
+
+    public function login() {
+
+        $error = false;
+
+        $username = $this->session->userdata('username');
+        $password = $this->session->userdata('password');
+
+        
+
+        if ($username == "dhika" && $password == "123") {
+            $_SESSION['$username'] = $username;
+            redirect("$this->load->view('home.php')");
+        } else {
+            $error == true;
+        }
+    }
+}
